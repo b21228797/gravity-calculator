57e54f8216076119287fe3cc9a63bf1f71b09173
b21228797 <32814734+b21228797@users.noreply.github.com>
2018-10-29 15:56:16 +0300

    Add files via upload

---
+++ quiz1.py
@@ -0,0 +1,10 @@
+#write  throught to code input values
+print("welcome to the gravity calculatios:")
+FT=float(input("Please enter the following time:"))
+GV=float(input("please enter the gravity constant:"))
+İV=0    //
+A=GV*FT*FT      //multply operations
+B=İV*FT+İP
+
+output = float((A/2)+B)
+print("The final position :",output)
